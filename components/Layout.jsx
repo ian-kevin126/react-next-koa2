0274ab17a4c9de1ccb64f0b4c4ec3c548eeeb8c3
ian_kevin126 <ian_kevin126@126.com>
2021-10-17 13:28:02 +0800

    feat：自定义App

---
+++ components/Layout.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import {Button} from 'antd'
+
+export default ({children}) => (
+  <header>
+    <Link href="/a?id=1" as="/a/1">
+      <Button type="primary">A</Button>
+    </Link>
+    <Link href="/b?id=1" as="/b/1">
+      <Button>B</Button>
+    </Link>
+  </header>
+)
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,71 @@
-import '../styles/globals.css'
+import App, { Container } from 'next/app'
+import 'antd/dist/antd.css'
+import React from 'react'
+import Router from 'next/router'
+import Layout from '../components/Layout'
+import PageLoading from '../components/PageLoading'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+  // 自定义App组件必须要做的步骤——重写getInitialProps方法，App组件的getInitialProps比较特殊，能拿到一些额外的参数
+  // 注意的是：每次页面切换都会执行这个方法
+  // Component: 被包裹的组件
+  static async getInitialProps(ctx) {
+    const { Component } = ctx
+    let pageProps = {}
+
+    // 拿到Component上定义的getInitialProps
+    if (Component.getInitialProps) {
+      // 执行拿到返回结果`
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    // 返回给组件
+    return {
+      pageProps,
+    }
+  }
+
+  state = {
+    context: 'value',
+    loading: false,
+  }
+
+  startLoading = () => {
+    this.setState({
+      loading: true,
+    })
+  }
+
+  stopLoading = () => {
+    this.setState({
+      loading: false,
+    })
+  }
+
+  componentDidMount() {
+    Router.events.on('routeChangeStart', this.startLoading)
+    Router.events.on('routeChangeComplete', this.stopLoading)
+    Router.events.on('routeChangeError', this.stopLoading)
+  }
+
+  componentWillUnmount() {
+    Router.events.off('routeChangeStart', this.startLoading)
+    Router.events.off('routeChangeComplete', this.stopLoading)
+    Router.events.off('routeChangeError', this.stopLoading)
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <Container>
+          <Layout>
+            {this.state.loading && <PageLoading />}
+            {/* 把pageProps解构后传递给组件 */}
+            <Component {...pageProps} />
+          </Layout>
+      </Container>
+    )
+  }
 }
 
 export default MyApp
- 
