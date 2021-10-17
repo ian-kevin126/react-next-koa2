01f52c3f1ef46ca05905d3dd04dbd09220d2ef9a
ian_kevin126 <ian_kevin126@126.com>
2021-10-17 14:02:19 +0800

    feat: 引入styled-components

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,13 +1,16 @@
-import Link from 'next/link';
-import {Button} from 'antd'
+import Link from 'next/link'
+import { Button } from 'antd'
 
-export default ({children}) => (
-  <header>
-    <Link href="/a?id=1" as="/a/1">
-      <Button type="primary">A</Button>
-    </Link>
-    <Link href="/b?id=1" as="/b/1">
-      <Button>B</Button>
-    </Link>
-  </header>
+export default ({ children }) => (
+  <>
+    <header>
+      <Link href="/a?id=1" as="/a/1">
+        <Button type="primary">A</Button>
+      </Link>
+      <Link href="/b?id=1" as="/b/1">
+        <Button>B</Button>
+      </Link>
+    </header>
+    {children}
+  </>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import styles from '../styles/Home.module.css'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: yellow;
+  font-size: 40px;
+`
 
 export default function Home() {
   return (
     <div className={styles.container}>
       1111
+      <Title>2222</Title>
     </div>
   )
 }
